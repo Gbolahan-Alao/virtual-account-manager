24061aed3b5ad3dbf1535b3c190e68d4341913b9
Alao <gbolahanlao@gmail.com>
2024-03-08 21:22:17 +0100

    Error Solved

--- src/routes/sections.jsx
+++ src/routes/sections.jsx
@@ -1,35 +1,43 @@
-import { lazy } from 'react';
-import { Navigate, useRoutes } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
+import { Navigate, Outlet, useRoutes } from 'react-router-dom';
+
 import DashboardLayout from 'src/layouts/dashboard';
-import ProtectedRoute from './components/ProtectedRoute';
+
 
 export const IndexPage = lazy(() => import('src/pages/app'));
 export const BlogPage = lazy(() => import('src/pages/blog'));
 export const UserPage = lazy(() => import('src/pages/user'));
-export const AppPage = lazy(() => import('src/pages/app'));
 export const LoginPage = lazy(() => import('src/pages/login'));
-export const CreateAccountPage = lazy(() => import('src/pages/create-account'));
-export const FindTransactionsPage = lazy(() => import('src/pages/find-transaction'));
+export const CreateAccountPage = lazy(()=> import('src/pages/create-account'));
+export const FindTransactionsPage = lazy(()=> import('src/pages/find-transaction'));
 export const ProductsPage = lazy(() => import('src/pages/products'));
 export const Page404 = lazy(() => import('src/pages/page-not-found'));
+export const AppPage = lazy(() => import('src/pages/app'));
 export const SettingsPage = lazy(() => import('src/pages/settings'));
 
+// ----------------------------------------------------------------------
 export default function Router() {
- const routes = useRoutes([
+  const routes = useRoutes([
     {
       path: '/',
       element: <LoginPage />,
     },
     {
-      path: 'app',
-      element: <DashboardLayout />,
+      element: (
+        <DashboardLayout>
+          <Suspense>
+            <Outlet />
+          </Suspense>
+        </DashboardLayout>
+      ),
       children: [
-        { path: 'user', element: <ProtectedRoute element={<UserPage />} /> },
-        { path: 'products', element: <ProtectedRoute element={<ProductsPage />} /> },
-        { path: 'blog', element: <ProtectedRoute element={<BlogPage />} /> },
-        { path: 'create-account', element: <ProtectedRoute element={<CreateAccountPage />} /> },
-        { path: 'settings', element: <ProtectedRoute element={<SettingsPage />} /> },
-        { path: 'transactions/:accountNumber', element: <ProtectedRoute element={<FindTransactionsPage />} /> },
+        { path: 'app', element: <AppPage /> },
+        { path: 'user', element: <UserPage /> },
+        { path: 'products', element: <ProductsPage /> },
+        { path: 'blog', element: <BlogPage /> },
+        { path: 'create-account', element: <CreateAccountPage /> },
+        { path: 'settings', element: <SettingsPage /> },
+         { path: 'transactions/:accountNumber', element: <FindTransactionsPage /> },
       ],
     },
     {
@@ -40,7 +48,8 @@ export default function Router() {
       path: '*',
       element: <Navigate to="/404" replace />,
     },
- ]);
+  ]);
 
- return routes;
+  return routes;
 }
+
